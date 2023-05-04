e17a2be52b600a937aaf32441eb288490dd20fc5
Boubaker Chieb <boubaker.ch@outlook.fr>
2023-05-04 23:13:35 +0100

    first commit

---
+++ src/data.js
@@ -0,0 +1,61 @@
+export const pageLinks = [
+  {
+    id: 1,
+    href: "#home",
+    text: "Home",
+  },
+  {
+    id: 2,
+    href: "#about",
+    text: "About",
+  },
+  {
+    id: 3,
+    href: "#services",
+    text: "Services",
+  },
+  {
+    id: 4,
+    href: "#tours",
+    text: "Tours",
+  },
+];
+
+export const socialLinks = [
+  {
+    id: 1,
+    icon: "fa-facebook",
+    href: "https://www.facebook.com",
+  },
+  {
+    id: 2,
+    icon: "fa-twitter",
+    href: "https://www.facebook.com",
+  },
+  {
+    id: 3,
+    icon: "fa-squarespace",
+    href: "https://www.facebook.com",
+  },
+];
+
+export const services = [
+  {
+    id: 1,
+    icon: "fas fa-wallet fa-fw",
+    title: "Saving money",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+  {
+    id: 2,
+    icon: "fas fa-tree fa-fw",
+    title: "endless hiking",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+  {
+    id: 3,
+    icon: "fas fa-socks fa-fw",
+    title: "Amazing confort",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+];
